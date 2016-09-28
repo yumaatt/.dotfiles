8a7c8c3cbf16c5e9f422c7bc7a5e769b00b104c3
yumaatt <yumaatt@gmail.com>
2016-09-29 00:23:13 +0900

    Fix ctrl+h in emacs

--- .emacs.d/inits/90-keybinds.el
+++ .emacs.d/inits/90-keybinds.el
@@ -7,3 +7,6 @@
 ;(key-chord-define-global "re" 'replace-string)
 ;(key-chord-define-global "zx" 'undo)
 ;(key-chord-define-global "df" 'descbinds-anything)
+
+(define-key global-map "\C-h" 'delete-backward-char) ; delete
+(define-key global-map "\M-?" 'help-for-help) ; help
